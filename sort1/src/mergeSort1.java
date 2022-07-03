e18e171ec3eda453fe98ab569e3a46327eb0cbe1
ruofengg <grfscott@hotmail.com>
2022-07-03 00:45:05 -0400

    mergesort

--- sort1/src/mergeSort1.java
+++ sort1/src/mergeSort1.java
@@ -1,8 +1,36 @@
+//just try
 public class mergeSort1{
     public static void mergeSort(int[] arr){
         mergeSort(arr, 0, arr.length - 1);
     }
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void mergeSort(int[] arr, int left, int right){
+        if(left < right){
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    public static void merge(int[] arr, int left, int mid, int right){
+        int[] temp = new int[right - left + 1];
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+        while(i <= mid && j <= right){
+            if(arr[i] < arr[j]){
+                temp[k++] = arr[i++];
+            }else{
+                temp[k++] = arr[j++];
+            }
+        }
+        while(i <= mid){
+            temp[k++] = arr[i++];
+        }
+        while(j <= right){
+            temp[k++] = arr[j++];
+        }
+        for(int l = 0; l < temp.length; l++){
+            arr[left + l] = temp[l];
+        }
     }
 }
